9b77461a0a935fb6b48171f9dc9c60d8df65f726
stnc1 <st1nc1.end@gmail.com>
2025-03-05 03:26:21 +0700

    added gx.nvim plugin

--- .config/nvim/lua/marjela/lazy/misc.lua
+++ .config/nvim/lua/marjela/lazy/misc.lua
@@ -17,5 +17,26 @@ return {
     },
     {
         "tpope/vim-repeat"
+    },
+    {
+        "chrishrb/gx.nvim",
+        keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+        cmd = { "Browse" },
+        init = function ()
+            vim.g.netrw_nogx = 1 -- disable netrw gx
+        end,
+        config = function() require("gx").setup({
+            handlers = {
+                github = false,
+                brewfile = false,
+                package_json = false,
+                jira = false,
+                rust = false
+            },
+            handler_options = {
+                search_engine = "https://search.brave.com/search?q="
+            }
+        })
+        end
     }
 }
--- .config/nvim/lua/marjela/set.lua
+++ .config/nvim/lua/marjela/set.lua
@@ -11,7 +11,7 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = true
-vim.opt.textwidth = 90
+vim.opt.textwidth = 100
 
 vim.opt.swapfile = false
 vim.opt.backup = false
