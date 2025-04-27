d68549eaf8d21535680cb5c3fe86e759a4ccec58
stnc1 <st1nc1.end@gmail.com>
2025-04-28 01:29:28 +0700

    Modified borders for telescope buffer

--- .config/nvim/lua/marjela/lazy/telescope.lua
+++ .config/nvim/lua/marjela/lazy/telescope.lua
@@ -1,16 +1,18 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-
         dependencies = {
-            "nvim-lua/plenary.nvim",                                        -- required
+            "nvim-lua/plenary.nvim",
             { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- for better perfomance
         },
-
         config = function()
             local telescope = require("telescope")
 
-            telescope.setup {
+            telescope.setup({
+                defaults = {
+                    borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+
+                },
                 pickers = {
                     find_files = {
                         hidden = true
@@ -19,7 +21,7 @@ return {
                 extensions = {
                     fzf = {},
                 }
-            }
+            })
 
             telescope.load_extension("fzf")
 
@@ -33,7 +35,7 @@ return {
             vim.keymap.set("n", "<leader>fh", function()
                 builtin.find_files({ cwd = os.getenv("HOME") })
             end, {})
-
+            -- find files in PWD
             vim.keymap.set("n", "<leader>ff", function()
                 if pcall(function()
                         builtin.git_files({
@@ -44,7 +46,7 @@ return {
                     builtin.find_files()
                 end
             end, {})
-
+            -- find help
             vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
         end
     }
